70fb5b09d06c968ed337f8c72a4649c03610cd66
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-28 02:52:06 +0400

    Creating a class LinkedList

--- Project10/LinkedList.h
+++ Project10/LinkedList.h
@@ -1,2 +1,19 @@
 #include <iostream>
 
+
+template <typename T>
+class LinkedList {
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+
+		Node() : data(), next(nullptr) {}
+		Node(const T& value) : data(value), next(nullptr){}
+	};
+
+	Node* head;
+	Node* tail;
+	int size;
+};
