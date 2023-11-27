1da5dd56c9914351c9bde9618e2bd1c131ed6cfd
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2023-11-28 03:02:55 +0400

    Add-to-end constructors

--- Project10/LinkedList.h
+++ Project10/LinkedList.h
@@ -16,4 +16,37 @@ private:
 	Node* head;
 	Node* tail;
 	int size;
+
+
+public:
+	LinkedList(): head(nullptr), tail(nullptr), size(0) {}
+
+	void push_tail(const T& value) {
+		Node* new_node = new Node(value);
+		++size;
+		if (head == nullptr) {
+			head = tail = new_node;
+			head->next = head;
+		}
+		else {
+			tail->next = new_node;
+			tail = new_node;
+			tail->next = head;
+		}
+	}
+
+	void push_tail(const LinkedList<T>& other) {
+		if (other.head != nullptr) {
+			Node* current_other = other.head;
+			int i = 0;
+			while (i<other.size)
+			{
+				push_tail(current_other->data);
+				current_other = current_other->next;
+				++i;
+			}
+		}
+	}
+
+
 };
